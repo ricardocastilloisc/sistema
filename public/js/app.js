cd7f405f8978e89831942a49e7eeb187eaf823a3
ricardocastilloisc <ricardocastilloisc@gmail.com>
2018-12-21 15:03:38 -0500

    5.021 Configurando el menu con VueJs

--- public/js/app.js
+++ public/js/app.js
@@ -48387,7 +48387,10 @@ Vue.component('example-component', __webpack_require__(/*! ./components/ExampleC
  */
 
 var app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    menu: 0
+  }
 });
 
 /***/ }),
