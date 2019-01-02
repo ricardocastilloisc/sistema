c30025fd50bf4b408443f088cfc9f204d3fdc04e
ricardocastilloisc <ricardocastilloisc@gmail.com>
2019-01-02 16:36:01 -0500

    Revert "7.038 Controlador ArticuloController.php y rutas"
    This reverts commit 06b6fc8ee672436ad52d305c5c31d7cd132708df.

--- database/migrations/2019_01_02_203920_create_articulos_table.php
+++ database/migrations/2019_01_02_203920_create_articulos_table.php
@@ -16,12 +16,12 @@ class CreateArticulosTable extends Migration
         Schema::create('articulos', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('idcategoria')->unsigned();
-            $table->string('codigo', 50)->nullable();
-            $table->string('nombre', 100)->unique();
-            $table->decimal('precio_venta', 11, 2);
+            $table->string('codigo',50)->nullable();
+            $table->string('nombre',100)->unique();
+            $table->decimal('precio_venta',11,2);
             $table->integer('stock');
-            $table->string('descripcion', 256)->nullable();
-            $table->boolean('condicion')->default(1);
+            $table->string('description', 256)->nullable();
+            $table->boolean('condicion')->default();
             $table->timestamps();
             $table->foreign('idcategoria')->references('id')->on('categorias');
         });
